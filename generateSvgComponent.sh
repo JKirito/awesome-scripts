e5f9ff96ead5f808012abdebfffdd5b23fd32927
JKirito <byrxarpitsingh@gmail.com>
2022-05-22 15:27:55 +0530

    Completed Dynamic Path

--- generateSvgComponent.sh
+++ generateSvgComponent.sh
@@ -14,9 +14,10 @@ content="$(cat folder.svg)"
 
 rm -f index.tsx
 touch index.tsx
-base="/home/jkirito/Desktop/testing/scripts"
-find $base -name "*.svg" > ds
-input="${base}/ds"
+# base="/home/jkirito/Desktop/testing/scripts"
+base=$1;
+find $base -name "*.svg" | tee $1ds
+input="${base}ds"
 # input="${base}/$(ls $1);"
 # echo "$input"
 while IFS= read -r line
@@ -24,17 +25,19 @@ do
   # echo "Content $line"
   # filename="${line:2}"
   filename=$(echo "${line}" | awk -F "/" '/^\// { print $NF }' | cut -f 1 -d '.')
-  filename=$(echo "${filename^}");
+  # filename=$(echo "${filename^}");
+  filename=$(echo "${filename^}" | sed 's/.*/\L&/; s/[a-z]*/\u&/g' | sed -e 's/\-//g');
   echo "${filename}"
-  content=$(cat "$line")
+  content=$(cat "$line" | sed -e 's/width="16"/width={width}/g' -e 's/height="16"/height={height} className={className}/g' -e 's/fill-rule/fillRule/g' -e 's/stroke-opacity/strokeOpacity/g' -e 's/clip-rule/clipRule/g')
   gtemplate="\n \
-const $filename = () => {\n \
-  return (\n \
-  $content \n \
-)\n \
-}\n \
+const $filename = (props: IIcon) => {
+  const { width, height, className } = props;
+  return (
+  $content
+)
+}
 ";
 
 printf "$gtemplate" >> index.tsx;
 done < "$input"
-rm ds;
+rm $1ds;
